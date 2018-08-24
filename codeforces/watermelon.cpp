a9698e09e2fe88dfdf0c8a9731e095aae6bab32d
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-08-24 11:42:46 -0300

    Add solution of watermelon

---
+++ codeforces/watermelon.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    (n%2!=0 or n==2) ? cout << "NO" << endl : cout << "YES" << endl;
+
+    return 0;
+}
