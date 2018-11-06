137514feaff38fabebade6b00a939c2f5ff644ff
Eduardo Nunes <eduardonunes2525@gmail.com>
2018-11-06 11:11:28 -0200

    Add magnets codeforce solution

---
+++ codeforces/exercises/magnets.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ll n; cin >> n; n--; 
+    string s, aux; cin >> aux;
+    int soma = 1;
+    while(n--){
+        cin >> s;
+        if(s!=aux){
+            soma++;
+            aux = s;
+        }
+    }
+
+    cout << soma << "\n";
+
+    return 0;
+}
