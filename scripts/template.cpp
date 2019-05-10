b0664429adddb767eb8b44b8c9f438bec1112d48
eduardonunes <eduardonunes2525@gmail.com>
2019-05-09 22:21:04 -0300

    Add new template

--- scripts/template.cpp
+++ scripts/template.cpp
@@ -1,13 +1,16 @@
 #include<bits/stdc++.h>
 
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define REP(i,a,b) for (int i = a; i <= b; i++)
+#define endl "\n"
 
 using namespace std;
 using ii = pair<int, int>;
 using ll = long long;
 
 int main(){
-    ios::sync_with_stdio(false); cin.tie(0);
+	IOS
+	
 
 
     return 0;
